9c499b4b6ad1bc87503d19e352d5bbdac8914142
Tammy George <georgetprecious@gmail.com>
2025-07-22 13:17:06 +0100

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -55,7 +55,7 @@ const Hero = () => {
       <div className="absolute top-1/2 left-6 transform -translate-y-1/2 z-20">
         <button
           onClick={prevVideo}
-          className="w-12 h-36 bg-transparent border border-[#fefefe]/0  flex items-center justify-center hover:bg-[#fefefe]/5 backdrop-blur-6xl transition-all duration-300"
+          className="w-12 h-36 bg-transparent border border-[#fefefe]/0  flex items-center justify-center hover:bg-[#fefefe]/5 backdrop-blur-4xl transition-all duration-300"
         >
           <ChevronLeft className="w-6 h-6 text-[#fefefe]/10" />
         </button>
@@ -64,7 +64,7 @@ const Hero = () => {
       <div className="absolute top-1/2 right-6 transform -translate-y-1/2 z-20">
         <button
           onClick={nextVideo}
-          className="w-12 h-36 bg-transparent border border-[#fefefe]/0  flex items-center justify-center hover:bg-[#fefefe]/5 backdrop-blur-6xl transition-all duration-300"
+          className="w-12 h-36 bg-transparent border border-[#fefefe]/0  flex items-center justify-center hover:bg-[#fefefe]/5 backdrop-blur-4xl transition-all duration-300"
         >
           <ChevronRight className="w-6 h-6 text-[#fefefe]/10" />
         </button>
