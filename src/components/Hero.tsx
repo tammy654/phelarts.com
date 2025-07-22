a0d6986c30a305a43e8586fe71042df6067d82fc
Tammy George <georgetprecious@gmail.com>
2025-07-22 13:12:10 +0100

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           onClick={prevVideo}
           className="w-12 h-36 bg-transparent border border-[#fefefe]/0  flex items-center justify-center hover:bg-[#fefefe]/5 backdrop-blur-2xl transition-all duration-300"
         >
-          <ChevronLeft className="w-6 h-6 text-[#fefefe]/0 hover:bg-[#fefefe]/5" />
+          <ChevronLeft className="w-6 h-6 text-[#fefefe]/0 hover:text-[#fefefe]/5" />
         </button>
       </div>
       
