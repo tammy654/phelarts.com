9f7fa162f83e9cf03b8004841527222afded7574
Tammy George <georgetprecious@gmail.com>
2025-08-23 10:04:56 +0100

    Updated Home.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -344,6 +344,7 @@ const About = () => {
       </ParallaxSection>
     </div>
   )
+  )
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -126,6 +126,7 @@ const Home = () => {
       </ParallaxSection>
     </div>
   )
+  )
   );
 };
 
